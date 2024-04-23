bd8701c09288865c71e4ded6236c6dca12f6eba2
Olumide <officialdebol@gmail.com>
2024-04-23 01:00:43 +0100

    My New GitHub Project

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,8 @@ import Header from "./components/Header";
 import AboutMe from "./components/AboutMe";
 import Repositories from "./components/Repositories";
 import "./App.css";
-import Breadcrumbs from "./components/Breadcrumbs";
+import Breadcrumbs from "./components/Breadcrumbs"
 import RepoSearch from "./components/RepoSearch";
-import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -16,7 +15,6 @@ function App() {
         <AboutMe />
         <Repositories />
         <RepoSearch />
-        <Footer />
       </div>
     </ChakraProvider>
   );
